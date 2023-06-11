685d2d90a962647bd28577de3ed6c7a9d3882721
Ashish <deshpandeashish90@gmail.com>
2023-06-11 21:07:25 +0530

    Updated main.tf

---
+++ 02-Read Generate Modify Configurations/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "mydemos3" {
+  bucket = "my-tf-demo-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+output "mys3" {
+  value = aws_s3_bucket.mydemos3   
+}
