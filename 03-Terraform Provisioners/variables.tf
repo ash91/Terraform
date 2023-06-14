e9d8fe6163e3b6b2ba05748faeb2eddbb58527b1
Ashish <deshpandeashish90@gmail.com>
2023-06-14 11:56:26 +0530

    default variables set

--- 03-Terraform Provisioners/variables.tf
+++ 03-Terraform Provisioners/variables.tf
@@ -1,11 +1,11 @@
-# variable "vpn_ip" {
-#   default = "116.50.30.50/32"
-# }
+variable "vpn_ip" {
+  default = "116.50.30.50/32"
+}
 
 variable "instancetype" {
-  # default = "t2.micro"
+  default = "t2.micro"
 }
 
 variable "ami_id" {
-  # default = "ami-04a0ae173da5807d3"
+  default = "ami-04a0ae173da5807d3"
 }
