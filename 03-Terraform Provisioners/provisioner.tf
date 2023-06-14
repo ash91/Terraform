ed649f96508136e4b72e2e8f87712c3d4dbb095f
Ashish <deshpandeashish90@gmail.com>
2023-06-14 08:38:38 +0530

    Updated provisioner.tf

--- 03-Terraform Provisioners/provisioner.tf
+++ 03-Terraform Provisioners/provisioner.tf
@@ -12,6 +12,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_instance" "instance1" {
+  ami = "ami-04a0ae173da5807d3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DemoEC2"
+  }
+}
 resource "aws_codecommit_repository" "test" {
   repository_name = "MyTestRepository"
   description     = "This is the Sample App Repository"
