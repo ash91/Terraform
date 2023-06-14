df30db0b25dbb7c2ed41ea2b804d3407509101b7
Ashish <deshpandeashish90@gmail.com>
2023-06-14 11:27:48 +0530

    updated vars-sg.tf

--- 03-Terraform Provisioners/vars-sg.tf
+++ 03-Terraform Provisioners/vars-sg.tf
@@ -5,20 +5,20 @@ resource "aws_security_group" "var_demo" {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpn_ip]
   }
   ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpn_ip]
   }
 
   ingress {
     from_port = 53
     to_port = 53
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpn_ip]
   }
 
 }
