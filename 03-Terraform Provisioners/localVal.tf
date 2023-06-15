1a59e638139ae253562367d7ab4c968c9939de51
Ashish <deshpandeashish90@gmail.com>
2023-06-15 14:40:29 +0530

    added functions.tf

--- 03-Terraform Provisioners/localVal.tf
+++ 03-Terraform Provisioners/localVal.tf
@@ -1,24 +1,24 @@
-locals {
-  common_tags = {
-    Owner   = "Devops Team"
-    service = "backend"
-  }
-}
+# locals {
+#   common_tags = {
+#     Owner   = "Devops Team"
+#     service = "backend"
+#   }
+# }
 
-resource "aws_instance" "app-dev" {
-  ami           = var.ami_id
-  instance_type = var.instancetype
-  tags          = local.common_tags
-}
+# resource "aws_instance" "app-dev" {
+#   ami           = var.ami_id
+#   instance_type = var.instancetype
+#   tags          = local.common_tags
+# }
 
-resource "aws_instance" "db-dev" {
-  ami           = var.ami_id
-  instance_type = var.instancetype
-  tags          = local.common_tags
-}
+# resource "aws_instance" "db-dev" {
+#   ami           = var.ami_id
+#   instance_type = var.instancetype
+#   tags          = local.common_tags
+# }
 
-resource "aws_ebs_volume" "db-ebs" {
-  availability_zone = "us-east-1a"
-  size              = 8
-  tags              = local.common_tags
-}
+# resource "aws_ebs_volume" "db-ebs" {
+#   availability_zone = "us-east-1a"
+#   size              = 8
+#   tags              = local.common_tags
+# }
