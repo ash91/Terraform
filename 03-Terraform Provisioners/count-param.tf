b058b18217e9acdc31bc0f3b1cf85b8ebbbce92e
Ashish <deshpandeashish90@gmail.com>
2023-06-15 13:38:12 +0530

    Updated count-param.tf

--- 03-Terraform Provisioners/count-param.tf
+++ 03-Terraform Provisioners/count-param.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "instance1" {
   ami           = var.ami_id
   instance_type = var.instancetype
-}
+  count         = 3
 
-resource "aws_instance" "instance2" {
-  ami           = var.ami_id
-  instance_type = var.instancetype
+  tags = {
+    Name = "Demoinstance.${count.index}"
+  }
+
+
 }
