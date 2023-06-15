29bbb0093e5360f6de7c48a3c03966a3a2faf0a8
Ashish <deshpandeashish90@gmail.com>
2023-06-15 14:41:37 +0530

    Updated functions.tf

--- 03-Terraform Provisioners/funtions.tf
+++ 03-Terraform Provisioners/funtions.tf
@@ -2,24 +2,6 @@ locals {
   time = formatdate("DD MMM YYYY hh:mm ZZZ", timestamp())
 }
 
-variable "region" {
-  default = "ap-south-1"
-}
-
-variable "tags" {
-  type    = list(any)
-  default = ["firstec2", "secondec2"]
-}
-
-variable "ami" {
-  type = map(any)
-  default = {
-    "us-east-1"  = "ami-0323c3dd2da7fb37d"
-    "us-west-2"  = "ami-0d6621c01e8c2de2c"
-    "ap-south-1" = "ami-0470e33cd681b2476"
-  }
-}
-
 resource "aws_key_pair" "loginkey" {
   key_name   = "login-key"
   public_key = file("${path.module}/id_rsa.pub")
