9c15eeacc2e57ceec99c99aa02852510a78b7dc5
Ashish <deshpandeashish90@gmail.com>
2023-06-15 15:44:28 +0530

    Updated data-sources.tf

--- 03-Terraform Provisioners/data-source.tf
+++ 03-Terraform Provisioners/data-source.tf
@@ -1,19 +1,23 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-  owners      = ["amazon"]
+variable "ami_component" {}
 
+data "aws_ami" "selected" {
   filter {
-    name   = "name"
-    values = ["myami-*"]
+    name   = "state"
+    values = ["available"]
   }
+  filter {
+    name   = "tag:component"
+    values = ["${var.ami_component}"]
+  }
+
+  most_recent = true
 }
 
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.selected.id
+  instance_type = "t2.micro"
 
-resource "aws_instance" "instance1" {
-
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instancetype
-  tags = {
-    name = "data_instance"
+  tags {
+    Name = "HelloWorld"
   }
 }
