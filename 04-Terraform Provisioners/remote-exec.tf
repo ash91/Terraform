ba1577d94dabb0d1869e46db8d06d1b3c1ae2680
Ashish <deshpandeashish90@gmail.com>
2023-06-16 10:49:34 +0530

    Added keypair to resource

--- 04-Terraform Provisioners/remote-exec.tf
+++ 04-Terraform Provisioners/remote-exec.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-04a0ae173da5807d3"
   instance_type = "t2.micro"
+  key_name      = "Terraform-Key"
 
   tags = {
     Name = "tf-remote-exec"
@@ -9,16 +10,16 @@ resource "aws_instance" "web" {
   # Establishes connection to be used by all
   # generic remote provisioners (i.e. file/remote-exec)
   connection {
-    type     = "ssh"
-    user     = "root"
-    password = var.root_password
-    host     = self.public_ip
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("./Terraform-Key.pem")
+    host        = self.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
-      "puppet apply",
-      "consul join ${aws_instance.web.private_ip}",
+      "sudo amazon-linux-extras install nginx1",
+      "sudo systemctl start nginx",
     ]
   }
 }
