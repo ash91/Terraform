8d83207f1fae2660caa92b640ddd2eebb027549f
Ashish <deshpandeashish90@gmail.com>
2023-06-16 12:21:04 +0530

    Updated all terraform files

--- 03-Terraform Provisioners/vars-sg.tf
+++ 03-Terraform Provisioners/vars-sg.tf
@@ -1,12 +1,12 @@
 resource "aws_security_group" "var_demo" {
   name = "variables-demo"
 
-  # ingress {
-  #   from_port = 443
-  #   to_port = 443
-  #   protocol = "tcp"
-  #   cidr_blocks = [var.vpn_ip]
-  # }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpn_ip]
+  }
   ingress {
     from_port   = 80
     to_port     = 80
