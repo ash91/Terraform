31c4064c50364e37b737128095416fe74050a3d2
Ashish <deshpandeashish90@gmail.com>
2023-06-16 10:39:36 +0530

    Added remote-exec

---
+++ 04-Terraform Provisioners/remote-exec.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "web" {
+  ami           = "ami-04a0ae173da5807d3"
+  instance_type = "t2.micro"
+
+  # Establishes connection to be used by all
+  # generic remote provisioners (i.e. file/remote-exec)
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = var.root_password
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "puppet apply",
+      "consul join ${aws_instance.web.private_ip}",
+    ]
+  }
+}
